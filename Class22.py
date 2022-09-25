0a91dfd3668ddf7a7fa74d8debe516f5d8aa7f35
ChandraSSIT <114322534+ChandraSSIT@users.noreply.github.com>
2022-09-25 08:22:33 +0530

    Add files via upload

---
+++ Class22.py
@@ -0,0 +1,32 @@
+# Python
+# we can use this for development of projects (Web applications,AI,Machine learning,scripts(devops),cloud(AWS,
+# Azure,google cloud,pyspark)
+# Django ( HTML,CSS) => python
+# Test frame work => Automation Testing => Test API's, Test UI Application , Test cloud Applications ,
+# Test Networking applications, Test operating system application
+
+# API Testing ,Test UI Application
+# Pytest Framework , Selenium (UI Testing framework)
+# Testing Manual /Automation
+
+# Manual Testing
+
+# Smoke test => Test core functionality
+# Login page => Valid login
+# Regression Testing => In detail testing ,when ever new release (i.e new functionality or bug fix)
+# to make existing functionality is not broken
+# Login page=> Valid login
+# Invalid Login => Login with invalid credentials
+
+# Sanity testing => if developer fixes any bug or any new small functionality implemented we will test this
+# functionality.
+
+
+
+# API Testing ,Test UI Application
+# Pytest Framework , Selenium (UI Testing framework)
+# Testing Manual /Automation
+
+# UI(User interface) => FE (front end)
+# API( Application program interface) => Middle layer (Back end)
+# Database => Data will store here
